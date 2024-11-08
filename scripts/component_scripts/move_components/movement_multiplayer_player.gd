3d5b1a5da750907a8e31209ded53d4607f669a1e
Bestfold <fredrik.schou.andersen.hoved@gmail.com>
2024-11-09 00:41:48 +0100

    Change from server authoritative to client (position and rotation) successfull. Left to do is replicate states, animation, work out why the camera cant be changed when 2 viewports, and do final clean-up from server to client auth -change

--- scripts/component_scripts/move_components/movement_multiplayer_player.gd
+++ scripts/component_scripts/move_components/movement_multiplayer_player.gd
@@ -4,12 +4,19 @@ class_name MovementMultiplayerPlayer
 
 # Returns a movement vector where [x, z] = [move x axis, move z axis]
 func get_movement_input() -> Vector2:
-	return %InputSynchronizer.input_dir
+	if multiplayer.get_unique_id() == parent.player_id:
+		var input_dir := Input.get_vector("left", "right", "forward", "back")
+		return input_dir
+	return Vector2(0,0)
 
 # Returns true if character wants to jump
 func wants_jump() -> bool:
-	return %InputSynchronizer.do_jump
+	if multiplayer.get_unique_id() == parent.player_id:
+		return Input.is_action_pressed("jump")
+	return false
 
 # Returns true if character wants to run
 func wants_run() -> bool:
-	return %InputSynchronizer.do_run
+	if multiplayer.get_unique_id() == parent.player_id:
+		return Input.is_action_pressed("run")
+	return false
--- scripts/player_multiplayer.gd
+++ scripts/player_multiplayer.gd
@@ -3,9 +3,9 @@ extends CharacterBody3D
 class_name PlayerMultiplayer
 
 ## Player-controller for multiplayer version of player.operator
-## State machine initialization is only done on server.
-## Passes refrences down to state machine (on server)
-## Sets authority for input replication
+## State machine initialization on server and clients.
+## Passes refrences down to state machine
+## Sets authority for replication. Position, rotation and states replicate from owner-client
 
 # Child refrences
 @onready var spring_arm: SpringArm3D = %SpringArm3D
@@ -28,39 +28,33 @@ signal var_monitoring_2(value_to_monitor)
 #signal var_monitoring_4(value_to_monitor)
 #signal var_monitoring_5(value_to_monitor)
 
+# LOG
+
 # 08.11.24
-# Ok, så utfordringer med input og replication mellom server og client. 
-#  Siste jeg gjorde var å bytte om slik at state_machine.handle_input ikke har noe med "server-input"
-#  å gjøre, men heller input som replicate'es fra InputSynchronizer
-# Symptomer per nå er at både move input og look input ikke nullstilles når man ikke fører ny input,
-#  kamera settes ikke riktig, og mouse-input hos client roterer ingenting.operator. Jeg tror svaret
-#  ligger i at server-client forholdet er litt janky i min implementasjon. Skriv og tegn en oversikt, og
-#  gå over fra topp til bunn.
+# Bytta slik at client har authority over position og rotation. Nest er å replicate state til server,
+#  og det er noe rot med kamera. Virker som man kun kan ha ett current camera når man åpner 2 viewports?
+# Det er også mye rot igjen etter byttet fra server authoritative til client.
 
-# Lurer på om ikke jeg skal endre slik at client har authority over movement, rotation og state. Da må
-#  jeg endre slik at state machine kjører på clients.
-
-
+# END LOG
 
 
 # I don't understand this yet. But it sets the player id whenever you set the id X)
 var player_id:
 	set(id):
 		player_id = id
-		# Replication-authority for the input replicater is set to this client, 
-		#  meaning: This clients input will be replicated to server, not the other way
+		# Replication-authority
 		%InputSynchronizer.set_multiplayer_authority(id)
+		%PlayerClientAuthSynchronizer.set_multiplayer_authority(id)
 
 # Player is controlled by state-children, as exception to common standard.
 
 func _ready() -> void:
-	# Initialize state machine only on server. Replicate game state to clients
-	if multiplayer.is_server():
-		# Initialize state machine, passing a refrence of player to the states
-		state_machine.init(self, animation_player, move_component, look_component,
-				can_interact_component)
+	# Initialize state machine, passing a refrence of player to the states
+	state_machine.init(self, animation_player, move_component, look_component,
+			can_interact_component)
+		
 	
-	# If this game instance's multiplayer id matches player's
+	# Run on client who owns player
 	if multiplayer.get_unique_id() == player_id:
 		camera.make_current()
 		#look_component.capture_mouse()
@@ -71,27 +65,26 @@ func _ready() -> void:
 	#	camera.current = false
 	
 
-func _unhandled_input(_event: InputEvent) -> void:
-	#if multiplayer.is_server():
+func _unhandled_input(event: InputEvent) -> void:
+	# Runs input on owning client
+	if multiplayer.get_unique_id() == player_id:
 		# Passes _unhandled_input() to state machine
-	#	state_machine.process_input(event)
+		state_machine.process_input(event)
 	
+	# Debug quit
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
+
 func _physics_process(delta: float) -> void:
-	if multiplayer.is_server():
-		# Passing function
-		state_machine.process_physics(delta)
+	# Passing function
+	state_machine.process_physics(delta)
 
-		var event: InputEvent = %InputSynchronizer.event
-		state_machine.process_input(event)
-
-		# Monitoring
-		#var_monitoring.emit(animation_tree.get("parameters/BlendSpace1D/blend_position"))
-		if look_component is LookPlayer:
-			var_monitoring_1.emit(look_component.head_bobbing_vector)
-			var_monitoring_2.emit(head_bobbing.position)
+	# Monitoring
+	#var_monitoring.emit(animation_tree.get("parameters/BlendSpace1D/blend_position"))
+	if look_component is LookPlayer:
+		var_monitoring_1.emit(look_component.head_bobbing_vector)
+		var_monitoring_2.emit(head_bobbing.position)
 
 # Passing function
 func _process(delta: float) -> void:
