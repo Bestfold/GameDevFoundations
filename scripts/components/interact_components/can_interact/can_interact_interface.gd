181c2275167192dd82cd82b594c60d2ccd73cc4e
Bestfold <fredrik.schou.andersen.hoved@gmail.com>
2024-11-13 22:59:49 +0100

    small tweaks to interaction system

--- scripts/components/interact_components/can_interact/can_interact_interface.gd
+++ scripts/components/interact_components/can_interact/can_interact_interface.gd
@@ -20,7 +20,7 @@ func _physics_process(_delta):
 			message = colliding_interactable.interact_prompt
 		
 			if get_request_for_interaction():
-				colliding_interactable.execute_interaction()
+				colliding_interactable.execute_interaction(parent)
 
 	if text_label:
 		# Debug
--- scripts/components/interact_components/interactable/test_interactable.gd
+++ scripts/components/interact_components/interactable/test_interactable.gd
@@ -1,4 +1,8 @@
 extends InteractableInterface
+class_name InteractableTest
 
-func execute_interaction():
+func execute_interaction(interacter):
 	print("Test interactable interacted B>")
+
+	if interacter is CharacterBody3D:
+		interacter.position = MultiplayerManager.respawn_point
